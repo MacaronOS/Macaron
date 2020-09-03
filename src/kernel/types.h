b2e0792a0865f7e6fdc42c82d90e139dfa0079e8
Plunkerusr <slava.pach@gmail.com>
2020-09-03 21:43:42 +0300

    Types: define some of the commonly used types

---
+++ src/kernel/types.h
@@ -0,0 +1,13 @@
+#ifndef MISTIX_KERNEL_TYPES_H
+#define MISTIX_KERNEL_TYPES_H
+
+typedef unsigned int   uint32_t;
+typedef unsigned short uint16_t;
+typedef unsigned char  uint8_t;
+
+typedef signed int int32_t;
+typedef long long int64_t;
+
+typedef int size_t;
+
+#endif // MISTIX_KERNEL_TYPES_H
