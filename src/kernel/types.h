9f344e7dd5bec38e2dc8fc2e1ce50e3161c7bb3b
Plunkerusr <slava.pach@gmail.com>
2020-09-14 22:36:46 +0300

    Types: add more types

--- src/kernel/types.h
+++ src/kernel/types.h
@@ -1,14 +1,20 @@
 #ifndef MISTIX_KERNEL_TYPES_H
 #define MISTIX_KERNEL_TYPES_H
 
+typedef unsigned long  uint64_t;
 typedef unsigned int   uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char  uint8_t;
-typedef char bool;
 
-typedef signed int int32_t;
-typedef long long int64_t;
+typedef long           int64_t;
+typedef signed int     int32_t;
+typedef signed short   int16_t;
+typedef signed char    int8_t;
 
-typedef int size_t;
+typedef char           bool;
+typedef int            size_t;
+
+#define false 0
+#define true 1
 
 #endif // MISTIX_KERNEL_TYPES_H
