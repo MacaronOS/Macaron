a04524a6185af8b575297c16d1113f51e60dc136
Plunkerusr <slava.pach@gmail.com>
2020-09-26 21:13:36 +0300

    PMM: fix incorrect memory assignment

--- src/kernel/memory/pmm.cpp
+++ src/kernel/memory/pmm.cpp
@@ -1,20 +1,21 @@
-#include "types.hpp"
-#include "regions.hpp"
 #include "pmm.hpp"
 
-#include "algo/bitmap.hpp"
-#include "../memory/memory.hpp"
-#include "../monitor.hpp"
+#include "regions.hpp"
+#include "memory.hpp"
+
+#include "../types.hpp"
 #include "../multiboot.hpp"
+
+#include "../algo/bitmap.hpp"
 
 #define KB_TO_BLOCKS(x) (x * KB / BLOCK_SIZE)
 
-bitmap_t pmmap; // contains the state of each memory block (0 - unused, 1 - used)
+kernel::algorithms::Bitmap pmmap; // contains the state of each memory block (0 - unused, 1 - used)
 
 void pmm_init_region(uint32_t base, size_t size)
 {
     for (size_t block = base / BLOCK_SIZE; block < base / BLOCK_SIZE + size / BLOCK_SIZE; block++) {
-        bitmap_set_false(&pmmap, block);
+        pmmap.set_false(block);
     }
 }
 
@@ -33,25 +34,26 @@ void pmm_init_available_regions(multiboot_info_t* multiboot_structure)
 
 void pmm_deinit_region(uint32_t base, size_t size) {
         for (size_t block = base / BLOCK_SIZE; block < base / BLOCK_SIZE + size / BLOCK_SIZE; block++) {
-        bitmap_set_true(&pmmap, block);
+            pmmap.set_true(block);
     }
 }
 
 void* pmm_allocate_block(ShouldZeroFill s)
 {
-    uint32_t block = bitmap_find_first_zero(&pmmap);
+    uint32_t block = pmmap.find_first_zero();
 
     if (block == BITMAP_NULL) {
         return 0;
     }
     memset((void*)(block * BLOCK_SIZE), 0, 4096);
-    bitmap_set_true(&pmmap, block); // mark this block as occupied
+    pmmap.set_true(block);
+
     return reinterpret_cast<void*>(block * BLOCK_SIZE); // return the physical locatiin of the available block
 }
 
 void pmm_free_block(void* block_address)
 {
-    bitmap_set_false(&pmmap, (uint32_t)block_address / BLOCK_SIZE);
+    pmmap.set_false((uint32_t)block_address / BLOCK_SIZE);
 }
 
 void pmm_init(multiboot_info_t* mb_structure)
@@ -60,9 +62,10 @@ void pmm_init(multiboot_info_t* mb_structure)
     const uint32_t blocks_size = mem_size / BLOCK_SIZE; // how much blocks of memory we manage
 
     // initializing pmmap bitmap right after the kernel
-    const uint32_t bitmap_memory_size = bitmap_init_at_location(&pmmap, blocks_size, get_kernel_pmm_bitmap_start());
+    pmmap = kernel::algorithms::Bitmap(get_kernel_pmm_bitmap_start(), blocks_size);
+    const uint32_t bitmap_memory_size = pmmap.size();
 
-    bitmap_set_all(&pmmap, 1); // for now, all blocks are inaccessible
+    pmmap.fill();
 
     pmm_init_available_regions(mb_structure); // sets free memory blocks in pmmap
 
@@ -72,7 +75,7 @@ void pmm_init(multiboot_info_t* mb_structure)
         kernel_block_address < get_kernel_end() ;
         kernel_block_address += BLOCK_SIZE
     ) {
-        bitmap_set_true(&pmmap, kernel_block_address / BLOCK_SIZE);
+        pmmap.set_true(kernel_block_address / BLOCK_SIZE);
     }
 
     // same for the kernel stack
@@ -81,7 +84,7 @@ void pmm_init(multiboot_info_t* mb_structure)
         kernel_stack_block_address < get_kernel_stack_end();
         kernel_stack_block_address += BLOCK_SIZE
     ) {
-        bitmap_set_true(&pmmap, kernel_stack_block_address / BLOCK_SIZE);
+        pmmap.set_true(kernel_stack_block_address / BLOCK_SIZE);
     }
 
     // and the same for the pmmap location
@@ -93,7 +96,7 @@ void pmm_init(multiboot_info_t* mb_structure)
 
     // then set this blocks to occypied
     for (uint32_t cur_bitmap_block = 0; cur_bitmap_block < bitmap_block_size; cur_bitmap_block++) {
-        bitmap_set_true(&pmmap, (uint32_t)get_kernel_pmm_bitmap_start() / BLOCK_SIZE + cur_bitmap_block);
+        pmmap.set_true((uint32_t)get_kernel_pmm_bitmap_start() / BLOCK_SIZE + cur_bitmap_block);
     }
 
 }
