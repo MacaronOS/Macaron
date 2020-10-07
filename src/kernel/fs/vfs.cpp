0a281c15ff4740a19a9cc8e8bb463ee230e9e800
Plunkerusr <slava.pach@gmail.com>
2020-10-07 19:15:41 +0300

    VFS: implement VFS

--- src/kernel/fs/vfs.cpp
+++ src/kernel/fs/vfs.cpp
@@ -1,13 +1,91 @@
 #include "vfs.hpp"
+#include "../algo/extras.hpp"
+#include "../monitor.hpp"
 
 namespace kernel::fs {
 
+VFS::VFS()
+{
+    m_root = File(FileType::Directory);
+    m_root.set_permission(FilePermission::Read);
+}
+
+void VFS::mount(File& dir, File& appended_dir)
+{
+    dir.mount(&appended_dir);
+}
+
 uint32_t VFS::read(const File& file, uint32_t offset, uint32_t size, void* buffer)
 {
-    if (file.read) {
-        return file.read(file, offset, size, buffer);
+    if (file.fs()) {
+        file.fs()->read(file, offset, size, buffer);
     }
+
     return 0;
+}
+
+uint32_t VFS::write(const File& file, uint32_t offset, uint32_t size, void* buffer)
+{
+    if (file.fs()) {
+        file.fs()->write(file, offset, size, buffer);
+    }
+
+    return 0;
+}
+
+File* VFS::finddir(const File& directory, const String& filename)
+{
+    if (directory.type() != FileType::Directory) {
+        return nullptr;
+    }
+
+    for (size_t i = 0; i < directory.mounted_dirs().size(); i++) {
+        if (directory.mounted_dirs()[i]->name() == filename) {
+            return directory.mounted_dirs()[i];
+        }
+    }
+
+    if (directory.fs()) {
+        return directory.fs()->finddir(directory, filename);
+    }
+
+    return nullptr;
+}
+
+Vector<File*> VFS::listdir(const File& directory)
+{
+    Vector<File*> result;
+    if (directory.type() != FileType::Directory) {
+        return result;
+    }
+
+    if (directory.fs()) {
+        result = directory.fs()->listdir(directory);
+    }
+
+    for (size_t i = 0; i < directory.mounted_dirs().size(); i++) {
+        result.push_back(directory.mounted_dirs()[i]);
+    }
+
+    return result;
+}
+
+File& VFS::create(const File& directory, File& file)
+{
+    if (directory.type() == FileType::Directory && directory.fs()) {
+        return directory.fs()->create(directory, file);
+    }
+
+    return file;
+}
+
+bool VFS::erase(const File& directory, const File& file)
+{
+    if (directory.type() == FileType::Directory && directory.fs()) {
+        return directory.fs()->erase(directory, file);
+    }
+
+    return false;
 }
 
 }
--- src/kernel/fs/vfs.hpp
+++ src/kernel/fs/vfs.hpp
@@ -1,11 +1,27 @@
 #pragma once
+#include "File.hpp"
 #include "fs.hpp"
 
 namespace kernel::fs {
 
 class VFS : public FS {
 public:
+    VFS();
+    ~VFS() = default;
+
+    File& root() override { return m_root; }
+
+    void mount(File& dir, File& appended_dir);
+
     uint32_t read(const File& file, uint32_t offset, uint32_t size, void* buffer) override;
+    uint32_t write(const File& file, uint32_t offset, uint32_t size, void* buffer) override;
+    File* finddir(const File& directory, const String& filename) override;
+    Vector<File*> listdir(const File& directory) override;
+    File& create(const File& directory, File& file) override;
+    bool erase(const File& directory, const File& file) override;
+
+private:
+    File m_root;
 };
 
 }
