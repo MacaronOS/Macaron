7977534b50e00e64a2dfefd6885a82f859e24443
Plunkerusr <slava.pach@gmail.com>
2020-10-12 22:59:12 +0300

    FS: change fs api functions

--- src/kernel/fs/fs.hpp
+++ src/kernel/fs/fs.hpp
@@ -16,13 +16,21 @@ public:
 
     virtual File& root() = 0;
 
-    virtual uint32_t read(const File& file, uint32_t offset, uint32_t size, void* buffer) { return 0; }
-    virtual uint32_t write(const File& file, uint32_t offset, uint32_t size, void* buffer) { return 0; }
+    virtual uint32_t read(File& file, uint32_t offset, uint32_t size, void* buffer) { return 0; }
+    virtual uint32_t write(File& file, uint32_t offset, uint32_t size, void* buffer) { return 0; }
+
     virtual void open(File& file, uint32_t offset, uint32_t size, uint8_t* buffer) { }
     virtual void close(File& file, uint32_t offset, uint32_t size, uint8_t* buffer) { }
-    virtual File* finddir(const File& directory, const String& filename) { }
-    virtual Vector<File*> listdir(const File& directory) {}
-    virtual File& create(const File& directory, File& file) { }
-    virtual bool erase(const File& directory, const File& file) { }
+
+    // returns file by filename, if it exists
+    virtual File* finddir(File& directory, const String& filename) { }
+
+    // returns all filenames inside the directory
+    virtual Vector<String> listdir(File& directory) { }
+
+    // creates a file inside file storage and returns it
+    virtual File& create(File& directory, const String& name, FileType type, file_permissions_t perms) { }
+
+    virtual bool erase(File& directory, const File& file) { }
 };
 }
