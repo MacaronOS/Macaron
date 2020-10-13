1d166f2d585c28c89363b020f1d43fd45c7c43c2
Plunkerusr <slava.pach@gmail.com>
2020-10-13 22:07:34 +0300

    FS: add file descriptor

--- src/kernel/fs/File.hpp
+++ src/kernel/fs/File.hpp
@@ -86,6 +86,8 @@ private:
 
 class File {
 public:
+    File() = default;
+    
     File(FS* fs, inode_t* inode_struct, uint32_t inode)
         : m_inode(inode)
         , m_inode_struct(inode_struct)
@@ -177,6 +179,31 @@ public:
 
 private:
     Vector<File*> files {};
+};
+
+#define O_RDONLY 0x1
+#define O_WRONLY 0x2
+#define O_RDWR 0x4
+#define O_DIRECTORY 0x8
+#define O_CREATE 0x10
+#define O_TRUNC 0x20
+
+class FileDescriptor {
+public:
+    FileDescriptor() = default;
+    
+    int flags() const { return m_flags; }
+    size_t offset() const { return m_offset; }
+
+    void add_flags(int flags) { m_flags |= flags; }
+
+    void inc_offset(size_t offset) { m_offset += offset; }
+    void dec_offset(size_t offset) { m_offset -= offset; }
+    void set_offset(size_t offset) { m_offset = offset; }
+
+private:
+    size_t m_offset {};
+    int m_flags {};
 };
 
 }
