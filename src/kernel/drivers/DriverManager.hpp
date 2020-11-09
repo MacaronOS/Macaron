4fd7ffc8ca2513e1d843c1c4530c187f85de29f1
Plunkerusr <slava.pach@gmail.com>
2020-11-09 21:12:02 +0300

    Managers: create managers on the heap

--- src/kernel/drivers/DriverManager.hpp
+++ src/kernel/drivers/DriverManager.hpp
@@ -3,19 +3,38 @@
 #include "Dirver.hpp"
 
 #include "../types.hpp"
+#include "../assert.hpp"
 
 #define MAX_DRIVERS 32
 
 namespace kernel::drivers {
 
 class DriverManager {
-private:
-    Driver m_drivers[MAX_DRIVERS] {};
-    uint8_t m_drivers_count { 0 };
-
 public:
+    DriverManager() = default;
+
+    static DriverManager* s_dm;
+    static bool initialized;
+    static bool initialize()
+    {
+        s_dm = new DriverManager();
+        DriverManager::initialized = true;
+        return DriverManager::initialized;
+    }
+    static DriverManager& the()
+    {
+        if (!DriverManager::initialized) {
+            ASSERT_PANIC("Driver manager referenced before initializing");
+        }
+        return *s_dm;
+    }
+
     void add_driver(Driver&);
     void install_all();
+
+private:
+    Driver* m_drivers[MAX_DRIVERS] {};
+    uint8_t m_drivers_count { 0 };
 };
 
 }
