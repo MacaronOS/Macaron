93da0b035bf0ccbdbc1ce8adad7254b18fb6e406
Plunkerusr <slava.pach@gmail.com>
2021-04-04 17:45:44 +0300

    wisterialib: add simple ObjectPool

---
+++ wisterialib/include/wisterialib/ObjectPool.hpp
@@ -0,0 +1,55 @@
+#pragma once
+#include "StaticStack.hpp"
+#include "Vector.hpp"
+#include "common.hpp"
+
+template <typename T, size_t size>
+class ObjectPool {
+public:
+    ObjectPool()
+    {
+        // TODO: support custom allocators, so kernel will be able to allocate this space more effectively
+        m_objects = (T*)malloc(sizeof(T) * size);
+    }
+
+    ~ObjectPool()
+    {
+        for (size_t obj_i = 0; obj_i < size; obj_i++) {
+            m_objects[obj_i].~T();
+        }
+        free(m_objects);
+        m_objects = nullptr;
+    }
+
+    ObjectPool(const ObjectPool&) = delete;
+    ObjectPool& operator=(const ObjectPool&) = delete;
+
+    struct AllocateResult {
+        size_t id {};
+        T* object {};
+    };
+
+    template <typename... Types>
+    AllocateResult allocate(Types... args)
+    {
+        if (!m_free_ids.size()) {
+            return {};
+        }
+        auto free_id = m_free_ids.top_and_pop();
+        new (&m_objects[free_id]) T(args...);
+        return {
+            .id = free_id,
+            .object = &m_objects[free_id],
+        };
+    }
+
+    void deallocate(const size_t id)
+    {
+        m_objects[id].~T();
+        m_free_ids.push(id);
+    }
+
+private:
+    T* m_objects;
+    StaticStack<size_t, size> m_free_ids {};
+};
