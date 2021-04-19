edf97583f887fb1287ecf8670b4fec65f35b92de
Plunkerusr <slava.pach@gmail.com>
2021-04-19 23:08:28 +0300

    WindowServer: add Screen

---
+++ userspace/apps/window_server/Screen.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <libgraphics/Bitmap.hpp>
+
+class Screen {
+    static constexpr uint32_t width = 1024;
+    static constexpr uint32_t height = 768;
+
+public:
+    Screen() = default;
+    Screen(uint8_t screen_fd, Graphics::Bitmap&& front_buffer, Graphics::Bitmap&& back_buffer);
+
+    Screen(const Screen&) = delete;
+    Screen& operator=(const Screen&) = delete;
+
+    Screen(Screen&&);
+    Screen& operator=(Screen&&);
+
+    Graphics::Bitmap& front_buffer();
+    const Graphics::Bitmap& front_buffer() const;
+
+    Graphics::Bitmap& back_buffer();
+    const Graphics::Bitmap& back_buffer() const;
+
+    void swap_buffers();
+
+private:
+    uint8_t m_screen_fd {};
+    Graphics::Bitmap m_first_buffer {};
+    Graphics::Bitmap m_second_buffer {};
+
+    Graphics::Bitmap* m_front_buffer {};
+    Graphics::Bitmap* m_back_buffer {};
+};
