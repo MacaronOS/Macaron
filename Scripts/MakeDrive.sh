39c719967b8b569534b1abad9f9e8455da7018e7
vpachkov <slava.pach@gmail.com>
2021-08-25 21:12:32 +0300

    Build: create drive only if necessary

--- Scripts/MakeDrive.sh
+++ Scripts/MakeDrive.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+if [ ! -f drive.img ]; then
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     EXT2_FORMATTER=mkfs
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     EXT2_FORMATTER=/usr/local/opt/e2fsprogs/sbin/mkfs.ext2
 fi
 
-rm -f drive.img
 qemu-img create -f raw drive.img 16M
 sudo $EXT2_FORMATTER -t ext2 -r 0 -b 1024 drive.img
 sudo mkdir -p mountpoint
@@ -14,3 +15,5 @@ sudo fuse-ext2 drive.img mountpoint -o rw+
 sudo touch mountpoint/file.txt
 sudo bash -c 'echo "testing..." > mountpoint/file.txt'
 sudo umount mountpoint
+
+fi
