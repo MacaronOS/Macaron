8a332437fc3f8251ac381fd5559a09fe6b465433
vpachkov <slava.pach@gmail.com>
2021-12-21 20:22:55 +0300

    [Kernel][Net] Use Ringbuffer in LocalSocket

--- Kernel/Tasking/Net/LocalSocket.hpp
+++ Kernel/Tasking/Net/LocalSocket.hpp
@@ -1,27 +1,19 @@
 #pragma once
 
-#include <Macaronlib/Common.hpp>
+#include <Macaronlib/Ringbuffer.hpp>
 
 namespace Kernel::net {
 
 class LocalSocket {
 public:
-    LocalSocket();
+    LocalSocket() = default;
 
-    // reads size bytes from the socket, return read offset
     uint32_t read(uint32_t offset, uint32_t size, uint8_t* buffer);
-
     void write(uint32_t size, const uint8_t* buffer);
-
     bool can_read(uint32_t offset) const;
 
-    inline uint32_t write_offset() { return m_write_offset; }
-
 private:
-    uint8_t* m_buffer;
-    uint32_t m_buffer_size;
-
-    uint32_t m_write_offset { 0 };
+    Ringbuffer<16 * 1024> m_buffer {};
 };
 
 }
