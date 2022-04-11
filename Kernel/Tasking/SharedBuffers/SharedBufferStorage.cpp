2f966528339c6fd4b06b7dd7ffb450dfd5174f06
vpachkov <slava.pach@gmail.com>
2022-04-11 21:32:13 +0300

    [Kernel][Tasking] Allocate shared buffers as VMArea objects

--- Kernel/Tasking/SharedBuffers/SharedBufferStorage.cpp
+++ Kernel/Tasking/SharedBuffers/SharedBufferStorage.cpp
@@ -20,7 +20,7 @@ CreateBufferResult SharedBufferStorage::create_buffer(uint32_t size)
     auto size_in_pages = (size + PAGE_SIZE - 1) / PAGE_SIZE;
 
     uint32_t start_frame = PMM::the().allocate_frames(size_in_pages);
-    uint32_t start_page = cur_proc->psized_find_free_space(size_in_pages).result();
+    uint32_t start_page = cur_proc->memory_description().allocate_memory_area<VMArea>(size, VM_READ | VM_WRITE).result()->vm_start() / PAGE_SIZE;
 
     region.object->frame = start_frame;
     region.object->pages = size_in_pages;
@@ -39,7 +39,7 @@ uint32_t SharedBufferStorage::get_buffer(uint32_t id)
     auto cur_proc = Scheduler::the().cur_process();
 
     auto region = m_free_regions.get(id);
-    auto start_page = cur_proc->psized_find_free_space(region->pages).result();
+    uint32_t start_page = cur_proc->memory_description().allocate_memory_area<VMArea>(region->pages * PAGE_SIZE, VM_READ | VM_WRITE).result()->vm_start() / PAGE_SIZE;
     cur_proc->psized_map(start_page, region->frame, region->pages, 0x7);
 
     return start_page * PAGE_SIZE;
