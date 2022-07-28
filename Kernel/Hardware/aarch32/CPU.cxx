8efd4ee933079efa7c06a07abc48550214c9d1fb
vpachkov <slava.pach@gmail.com>
2022-07-28 23:31:01 +0300

    [Kernel][ARM] Add required barriers to the flush tlb function

--- Kernel/Hardware/aarch32/CPU.cxx
+++ Kernel/Hardware/aarch32/CPU.cxx
@@ -4,6 +4,8 @@ namespace Kernel {
 
 void CPU::flush_tlb(uintptr_t virtual_address, size_t page_count)
 {
+    // After the translation tables update, any old copies of entries that
+    // might be held in the TLBs must be invalidated.
     while (page_count) {
         asm volatile(
             "mcr p15, 0, %0, c8, c7, 3" ::"r"(virtual_address)
@@ -12,6 +14,11 @@ void CPU::flush_tlb(uintptr_t virtual_address, size_t page_count)
         virtual_address += page_size();
         page_count--;
     }
+    // This operation is only guaranteed to affect all instructions,
+    // including instruction fetches and data accesses,
+    // after the execution of a DSB and an ISB.
+    asm volatile("dsb");
+    asm volatile("isb");
 }
 
 void CPU::enable_interrupts()
