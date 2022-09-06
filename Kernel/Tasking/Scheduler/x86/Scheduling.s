cc0ab2cb844fe45d8d209a95fdc7678450047918
vpachkov <slava.pach@gmail.com>
2022-09-06 20:20:27 +0300

    [Kernel][x86] Reorganize scheduling code

--- Kernel/Tasking/Scheduler/x86/Scheduling.s
+++ Kernel/Tasking/Scheduler/x86/Scheduling.s
@@ -1,37 +1,4 @@
-global switch_to_user
-switch_to_user:
-    mov esp, [esp+4] ; Trapframe* tf
-
-    pop eax
-    mov ds, ax
-
-    pop eax
-    mov es, ax
-
-    pop eax
-    mov fs, ax
-
-    pop eax
-    mov gs, ax
-
-    popad
-    add esp, 8
-    iretd
-
-global switch_to_kernel
-switch_to_kernel:
-    mov eax, [esp + 4] ; KernelContext** kc
-
-    mov esp, [eax]
-    mov DWORD[eax], 0 ; so the thread no longer thinks it's blocked
-
-    pop ebx
-    pop ebp
-    pop esi
-    pop edi
-
-    ret
-
+; void block_and_switch_to_kernel(KernelContext** cur, KernelContext** next)
 global block_and_switch_to_kernel
 block_and_switch_to_kernel:
     mov eax, [esp + 4] ; KernelContext** cur
@@ -46,6 +13,17 @@ block_and_switch_to_kernel:
     mov esp, [edx]
     mov DWORD[edx], 0 ; so the next thread no longer thinks it's blocked
 
+    jmp switch_to_kernel_out
+
+; void switch_to_kernel(KernelContext** kc)
+global switch_to_kernel
+switch_to_kernel:
+    mov eax, [esp + 4] ; KernelContext** kc
+
+    mov esp, [eax]
+    mov DWORD[eax], 0 ; so the thread no longer thinks it's blocked
+
+switch_to_kernel_out:
     pop ebx
     pop ebp
     pop esi
@@ -53,6 +31,7 @@ block_and_switch_to_kernel:
 
     ret
 
+; void block_and_switch_to_user(KernelContext** cur, Trapframe* next)
 global block_and_switch_to_user
 block_and_switch_to_user:
     mov eax, [esp + 4] ; KernelContext** cur
@@ -66,6 +45,14 @@ block_and_switch_to_user:
     mov [eax], esp
     mov esp, edx
 
+    jmp switch_to_user_out
+
+; void switch_to_user(Trapframe* tf)
+global switch_to_user
+switch_to_user:
+    mov esp, [esp+4] ; Trapframe* tf
+
+switch_to_user_out:
     pop eax
     mov ds, ax
 
